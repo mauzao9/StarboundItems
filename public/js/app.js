f1ef63e796d4aa0b52b5a87520ab4dfbaa5b2421
Jason Soares <soaresjd@gmail.com>
2015-04-08 06:34:26 -0700

    Minimal ember app + layout adjustments. Does not currently work.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
 var app = angular.module('starboundItems', []);
+
+var App = Ember.Application.create({
+  LOG_TRANSITIONS: true
+});
+
+App.Router.map(function () {
+  this.route('all');
+  this.route('stats');
+});
